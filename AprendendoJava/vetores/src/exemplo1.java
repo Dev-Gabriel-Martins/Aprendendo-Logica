56735cbe9e4bf621bf125bc38d36d5883c870fd0
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-08-07 09:48:06 -0300

    add vetores1 java

---
+++ AprendendoJava/vetores/src/exemplo1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class exemplo1 {
+        
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        int n = input.nextInt();
+        double[] vect = new double[n];
+        
+        for(int i=0; i <n; i++){
+            vect[i] = input.nextDouble();
+            
+        }
+        double sum = 0.0;
+        for(int i=0;i <n;i++){
+            sum += vect[i];
+        }
+        double avg = sum / n;
+        
+        System.out.println("Average height: " +avg);
+        
+    }
+}
