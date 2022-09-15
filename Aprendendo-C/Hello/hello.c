b6596f1a40e789ba6ce6cd826ff4457f82c39b22
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-15 12:04:45 -0300

    modificado hello.c
    Configurado a compilação do C / C++ no VS Code

---
+++ Aprendendo-C/Hello/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char nome[50];
+    strcpy(nome, "Gabriel");
+    printf("Olá mundo eu sou o  %s\n", nome);
+      printf("\n");
+        printf("\n");
+    return 0;
+
+}
--- Aprendendo-C/hello.c
+++
@@ -1,5 +0,0 @@
-# include <stdio.h>
-int main(){
-printf("Hello Word\n");
-return 0;
-}
