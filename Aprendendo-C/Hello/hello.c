e2f462bf14db348c6521a29cc92a472a05c4f27f
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-15 13:08:07 -0300

    Teste commit pelo vsCode

--- Aprendendo-C/Hello/hello.c
+++ Aprendendo-C/Hello/hello.c
@@ -2,11 +2,14 @@
 #include <string.h>
 
 int main(){
+   
+
     char nome[50];
     strcpy(nome, "Gabriel");
     printf("Olá mundo eu sou o  %s\n", nome);
-      printf("\n");
-        printf("\n");
+       
+    printf("\n");
+    printf("\n");
     return 0;
 
 }
