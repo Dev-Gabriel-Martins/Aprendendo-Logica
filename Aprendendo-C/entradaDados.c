499c1b75d7ae478a674e06ee0ac9050cbe517664
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-15 16:04:48 -0300

    Update entradaDados.c

--- Aprendendo-C/entradaDados.c
+++ Aprendendo-C/entradaDados.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include "gabsLib.h"
 
 int main(){
     int idade;
-    double salario, altura;    
+    double salario, altura;
+    char nome[50];
+
+    printf("Digite seu  nome: ");
+    ler_texto(nome ,50);
 
     printf("Digite o valor da idade: ");
     scanf("%d", &idade);
@@ -14,12 +19,13 @@ int main(){
     printf("Digite o valor da altura: ");
     scanf("%lf", &altura);
 
-    printf("\n");
+
     
+    printf("NOME = %s\n", nome);
     printf("IDADE = %d\n", idade);
     printf("SALARIO = %.2lf\n", salario);
     printf("ALTURA = %.2lf\n", altura);
     
-    printf("\n");
+    close();
     return 0;
 }
