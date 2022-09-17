e7b3374eec205628062b5a204056c0c46ebfdbf0
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-17 14:05:36 -0300

    add somar_vetor.c

---
+++ Aprendendo-C/soma_vetor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    double soma, media, cont;
+    printf("Quantos numeros você vai digitar? \n");
+    scanf("%i",&n);
+    double vet[n];
+
+    for(int i = 0;i < n; i++){
+
+    printf("Digite um numero: ");
+    scanf("%lf",&vet[i]);
+    }
+
+    soma =0;
+
+    printf("Valores:");
+    for(int i = 0;i < n; i++){    
+    soma += vet[i];
+    cont++;
+    printf("%.1lf  ",vet[i]);
+    }
+    
+    media = soma / cont;
+    printf("\n Soma: %.1lf\n",soma);
+    printf("Media: %.1lf\n",media);
+
+
+   return 0; 
+}
