db027c92dc209c5517d6dd87e0643a2ed5dd8b6c
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-19 14:48:46 -0300

    add retangulo.cpp

---
+++ Aprendendo-C++/Exercícios/retangulo.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    double base, altura, area, perimetro, diagonal;
+
+    cout << "Digite a base do triangulo: " << endl;
+    cin >> base;
+
+    cout << "altura do triandogulo" << endl;
+    cin >> altura;
+
+    area = base * altura;
+
+    perimetro = 2*(base + altura);
+
+    diagonal = sqrt(pow(base,2.0) + pow(altura,2.0));
+
+    cout << fixed << setprecision(2);
+    cout << "Area: " << area << endl;
+    cout << "Perimetro: " << perimetro << endl;
+    cout << "Diagonal: " << diagonal << endl;
+    
+    return 0;
+}
