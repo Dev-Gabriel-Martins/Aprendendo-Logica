4c4e3e6f271a1814a4da100c1196498156ddbaf9
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-20 12:14:57 -0300

    add vetores.java

--- Aprendendo-Java/Exemplos/Exemplos/src/exemplos/repeticao_doWhile.java
+++ Aprendendo-Java/Exemplos/Exemplos/src/exemplos/repeticao_doWhile.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package exemplos;
 
-/**
- *
- * @author gabs
- */
+package exemplos;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class repeticao_doWhile {
+    public static void main(String[] args){
     
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        double C, F;
+        char resp;
+        
+        do {
+            System.out.print("Digite a temperatura em Celsius: ");
+            C = sc.nextDouble();
+            F = 9.0 * C / 5.0 + 32.0;
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+            System.out.print("Deseja repetir (s/n)? ");
+            resp = sc.next().charAt(0);
+        } while (resp == 's');
+        
+        sc.close();
+    
+    }
 }
---
+++ Aprendendo-Java/Exemplos/Exemplos/src/exemplos/vetores.java
@@ -0,0 +1,30 @@
+package exemplos;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class vetores {
+    public static void main(String[] args){
+    
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int n;
+        
+        System.out.println("Quantas numero voce vai digitar");
+        n = sc.nextInt();
+        
+        double[] vet = new double[n];
+        
+        for(int i=0; i < n; i++){
+            System.out.println("Digite um numero: ");
+            vet[i] = sc.nextDouble();
+        }
+        
+        System.out.println("\n numeros digitados:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(String.format("%.1f", vet[i]));
+        }
+        sc.close();    
+    }
+}
