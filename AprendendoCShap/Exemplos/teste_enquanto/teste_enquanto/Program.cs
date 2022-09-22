7cd41e058e07ec8fb4504f09dfacd644a3b47b2c
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-22 17:55:21 -0300

    add teste_enquanto.cs

---
+++ AprendendoCShap/Exemplos/teste_enquanto/teste_enquanto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Programa { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, soma;
+
+            soma = 0;
+            Console.Write("Digite o primeiro numero: ");
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0) {
+                soma = soma + x;
+                Console.Write("Digite outro numero: ");
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Soma= " + soma);
+        }
+
+
+    }
+    
+}
