4e6ff7783f9675ba722883c0d72bb061cb6e56f5
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-22 17:54:32 -0300

    add condicional

---
+++ AprendendoCShap/Exemplos/condicional/condicional/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class teste_condicional
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            int hora;
+            Console.WriteLine("Digite uma hora do dia");
+            hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia !");
+            }
+            else
+            {
+                Console.WriteLine("Boa tarde! ");
+            }
+        }
+    }
+}
+
+
