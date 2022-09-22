60b637fcffa95845c42c18456bccc824608a3b09
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-22 17:55:33 -0300

    add teste_for.cs

---
+++ AprendendoCShap/Exemplos/teste_para/teste_para/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Programa { 
+    class teste_para
+    {
+        static void Main(string[] args) {
+
+            Console.Write("Quantos numeros serão digitados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+
+            for (int i = 0; i < n; i++) { 
+            Console.Write("Digite um numero: ");
+                int x = int.Parse(Console.ReadLine());
+                soma += x;
+            }
+
+            Console.WriteLine("Soma =" + soma);
+
+        }
+    }
+    
+}
