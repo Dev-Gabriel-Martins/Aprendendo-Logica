d7c5556681f29df36466f5fbb4d6990db5e4a624
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-22 17:56:15 -0300

    add retangulo.cs

---
+++ AprendendoCShap/Exercício/retangulo/retangulo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+        
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            double base1, altura, area, perimetro, diagonal;
+
+            Console.WriteLine("Digite a base do retangulo: ");
+            base1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura do retangulo:");
+            altura = double.Parse(Console.ReadLine());
+
+            area = base1 * altura;
+
+            perimetro = (base1 + altura) * 2;
+
+            diagonal = Math.Sqrt(Math.Pow(base1,2.0) + Math.Pow(altura,2.0));
+
+            Console.WriteLine("Area: " + area.ToString("F2", culture));
+            Console.WriteLine("Perimetro: " + perimetro.ToString("F2", culture));
+            Console.WriteLine("Diagonal: " + diagonal.ToString("F2", culture));
+        }
+    }
+}
