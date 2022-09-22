c61c3ef33a266a6eff358cb6e205752f8b669c3c
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-22 17:55:47 -0300

    add vetores.cs

---
+++ AprendendoCShap/Exemplos/vetores/vetores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int N, i;
+            Console.Write("Quantos numeros voce vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+            double[] vet = new double[N];
+            for (i = 0; i < N; i++)
+            {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+            Console.WriteLine();
+            Console.WriteLine("NUMEROS DIGITADOS:");
+            for (i = 0; i < N; i++)
+            {
+                Console.WriteLine(vet[i].ToString("F1", CI));
+            }
+        }
+    }
+}
