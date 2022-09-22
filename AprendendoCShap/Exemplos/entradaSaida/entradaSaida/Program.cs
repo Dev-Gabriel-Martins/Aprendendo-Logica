1447a05b8b585612a2ec4b3b4b1ac7239f5e59f6
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-22 17:54:41 -0300

    Create Program.cs

---
+++ AprendendoCShap/Exemplos/entradaSaida/entradaSaida/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class entradaDados
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            double salario1, salario2;
+            string nome1, nome2;
+            int idade;
+            char sexo;
+
+            Console.WriteLine("Nome da primeira pessoa: ");
+            nome1 = Console.ReadLine();
+            Console.WriteLine("Salario da primeira pessoa: ");
+            salario1 = double.Parse(Console.ReadLine(), culture);
+
+
+            Console.WriteLine("Nome da segunda pessoa: ");
+            nome2 = Console.ReadLine();
+            Console.WriteLine("Salario da segunda pessoa: ");
+            salario2 = double.Parse(Console.ReadLine(), culture);
+
+            Console.WriteLine("Digite uma idade: ");
+            idade = int.Parse(Console.ReadLine(), culture);
+            Console.WriteLine("Digite um sexo (F/M): ");
+            sexo = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome 1: " + nome1);
+            Console.WriteLine("Salario 1: " + salario1.ToString("F2", culture));
+            Console.WriteLine("Nome 2: " + nome2);
+            Console.WriteLine("Salario 2: " + salario2.ToString("F2", culture));
+            Console.WriteLine("Idade " + idade);
+            Console.WriteLine("Sexo: " + sexo);
+        }
+    }
+}
+
