e98aa0178ec4b29a6623bb0547a30fb3a1d66758
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-22 17:55:06 -0300

    add teste_doWhile

---
+++ AprendendoCShap/Exemplos/teste_doWhile/teste_doWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double C, F;
+            char resp;
+            do
+            {
+                Console.Write("Digite a temperatura em Celsius: ");
+                C = double.Parse(Console.ReadLine(), CI);
+                F = 9.0 * C / 5.0 + 32.0;
+                Console.WriteLine("Equivalente em Fahrenheit: " +
+               F.ToString("F1", CI));
+                Console.Write("Deseja repetir (s/n)? ");
+                resp = char.Parse(Console.ReadLine());
+            } while (resp == 's');
+        }
+    }
+}
