9131e2354c18e123b440053ba5e95531248c0fb8
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-23 16:46:07 -0300

    add tipo.py

---
+++ Aprendendo-Python/Exemplos/tipo.py
@@ -0,0 +1,21 @@
+idade: int
+salario: float
+altura: float
+genero: str
+nome: str
+
+idade = 20
+salario = 5800.5
+altura = 1.53
+genero = "f"
+nome = "Maria Silva"
+
+print(f"Idade= {idade}")
+print(f"Salario= {salario:.2f}")
+print(f"Altura= {altura:.2f}")
+print(f"Genero= {genero}")
+print(f"Nome: {nome}")
+
+
+
+
