f1ee9df537d3fcd3b8079b7c214166abc14851c1
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-23 14:08:07 -0300

    add crescente.cs

---
+++ AprendendoCShap/Exercício/crescente/crescente/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Program
+{
+    class Crescente
+    {
+        static void Main(string[] args)
+        {
+            int primeiro, segundo;
+
+            do
+            {
+                Console.WriteLine("Digite dois numeros: ");
+                primeiro = int.Parse(Console.ReadLine());
+                segundo = int.Parse(Console.ReadLine());
+
+                if (primeiro > segundo)
+                {
+                    Console.WriteLine("Descrescente!");
+                }
+                else if( segundo > primeiro)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine(" ");
+                }
+
+            
+
+            } while (primeiro != segundo);
+
+
+        }
+    }
+}
