d893c99ed9788637369cb8fb4c20225a05deb542
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-23 14:09:09 -0300

    add soma_impares.cs

---
+++ AprendendoCShap/Exercício/soma_impares/soma_impares/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Program
+{
+    class soma_impares
+    {
+        static void Main(string[] args)
+        {
+            int x, y, somarImp, aux;
+            Console.WriteLine("Digite dois numeros");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                aux = x;
+                x = y;
+                y = aux;
+            }
+
+            somarImp = 0;
+
+            for (int i = x + 1; i < y; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    somarImp += i;
+                }
+            }
+
+            Console.WriteLine("Soma do Pares: " + somarImp);
+
+        }
+    }
+
+}
