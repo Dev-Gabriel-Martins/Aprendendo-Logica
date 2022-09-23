6b299d2eb01d55a2835e7c043647fa23ec7109f2
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-23 14:08:19 -0300

    add diagonal.cs

---
+++ AprendendoCShap/Exercício/diagonal_negativos/diagonal_negativos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Program
+{
+    class diagonal_negativos
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a ordem matriz? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n,n];
+
+            int contNeg = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Elemento ["+i+","+j+"]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Digaonal Principal: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if(matriz[i,j] < 0) {
+                        contNeg++;
+                    }
+                    
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("  "+ matriz[i,j]);
+                    }
+
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quantidade de negativos: "+contNeg);
+
+        }
+    }
+}
