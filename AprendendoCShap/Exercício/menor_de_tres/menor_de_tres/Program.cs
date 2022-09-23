95d703403e57cafaf1d653daac24f58e84c36242
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-23 14:08:43 -0300

    add menor_de_tres.cs

---
+++ AprendendoCShap/Exercício/menor_de_tres/menor_de_tres/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Program
+{
+    class menor_de_tres
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2, valor3;
+
+            Console.WriteLine("Primeiro valor: ");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            valor2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Terceiro valor: ");
+            valor3 = int.Parse(Console.ReadLine());
+
+            if(valor1 < valor2 && valor1 < valor3)
+            {
+                Console.WriteLine("Menor valor: "+valor1);
+            }else if(valor2 < valor3)
+            {
+                Console.WriteLine("Menor valor: "+valor2);
+            }
+            else
+            {
+                Console.WriteLine("Menor valor: "+valor3);
+            }            
+
+        }
+    }
+
+}
