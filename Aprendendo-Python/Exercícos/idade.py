f8e4ac6acc948a02243a4beef828b59c9d78971e
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-24 13:40:22 -0300

    idade.py

---
+++ Aprendendo-Python/Exercícos/idade.py
@@ -0,0 +1,11 @@
+print("Dados das primeira pessoa: ")
+nome = input("Nome: ")
+idade = int(input("Idade: "))
+
+print("Dados da segunda pessoa: ")
+nome2 = input("Nome: ")
+idade2 = int(input("Idade: "))
+
+media = (idade + idade2)/2
+
+print(f"A idade media da {nome} e {nome2} e de {media} anos")
