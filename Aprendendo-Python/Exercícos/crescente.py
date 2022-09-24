802af3daca90b76156b03e42c50f298bdff0da56
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-24 14:59:56 -0300

    add crescente.py

---
+++ Aprendendo-Python/Exercícos/crescente.py
@@ -0,0 +1,13 @@
+print("Digite dois numeros: ")
+x = int(input())
+y = int(input())
+
+while(x != y):
+    if(x > y):
+        print("Decrescente ")
+    else:
+        print("Crescente ")
+
+    print("Digite dois numeros: ")
+    x = int(input())
+    y = int(input())
